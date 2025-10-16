cee6fc4a46e7f5673778b6e8d9af0cdc9e4671ad
Jake-G123 <jakelgerken@gmail.com>
2025-10-16 12:42:56 -0700

    changed port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import express from 'express';
 const app = express();
 
 // Define the port number where our server will listen
-const PORT = 3001;
+const PORT = 3002;
 
 // Define a default "route" ('/')
 // req: contains information about the incoming request
